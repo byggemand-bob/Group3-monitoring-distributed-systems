ca477bafb127efb904fd0e4b954940b440314e11
Stensgaard <aralidan@outlook.com>
2021-03-02 09:00:41 +0100

    Update testScript.py
    fix

--- Server/testScript.py
+++ Server/testScript.py
@@ -7,7 +7,7 @@ data = data.replace('templateDir: src/main/resources/cstTemplates', 'templateDir
 #close the input file
 fin.close()
 #open the input file in write mode
-fin = open("config.yaml", "wt")
+fin = open("/home/travis/build/byggemand-bob/Group3-monitoring-distributed-systems/Server/config.yaml", "wt")
 #overrite the input file with the resulting data
 fin.write(data)
 #close the file
