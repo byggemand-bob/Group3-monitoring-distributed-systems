37ce66766c248d8407f577a86c9f8fa4a2d91134
Stensgaard <aralidan@outlook.com>
2021-03-02 08:54:27 +0100

    Update testScript.py
    made the python script to replace templateDir: src/main/resources/cstTemplates to templateDir: Server/src/main/resources/cstTemplates

--- Server/testScript.py
+++ Server/testScript.py
@@ -1,3 +1,14 @@
-#!/bin/sh
-# This is a comment!
-echo Hello World	# This is a comment, too!
+#read input file
+fin = open("config.yaml", "rt")
+#read file contents to string
+data = fin.read()
+#replace all occurrences of the required string
+data = data.replace('templateDir: src/main/resources/cstTemplates', 'templateDir: Server/src/main/resources/cstTemplates')
+#close the input file
+fin.close()
+#open the input file in write mode
+fin = open("config.yaml", "wt")
+#overrite the input file with the resulting data
+fin.write(data)
+#close the file
+fin.close()
