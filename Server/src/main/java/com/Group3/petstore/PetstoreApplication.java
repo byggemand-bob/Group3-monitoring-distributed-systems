60dd299ca2b3039160533fefefe64ca9f99ca1bd
Stensgaard <aralidan@outlook.com>
2021-03-03 08:21:22 +0100

    Update PetstoreApplication.java
    removed comment that was there for testing

--- Server/src/main/java/com/Group3/petstore/PetstoreApplication.java
+++ Server/src/main/java/com/Group3/petstore/PetstoreApplication.java
@@ -8,7 +8,6 @@ public class PetstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetstoreApplication.class, args);
-		// Awesome new comment, totally changing the way of coding!
 	}
 
 }
