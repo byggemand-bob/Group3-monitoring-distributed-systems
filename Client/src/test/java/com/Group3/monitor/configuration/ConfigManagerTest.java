b4728dcdb3e7b207339a97d77bee4c2bf83abdb9
AxialDark <alex.klitgaard@gmail.com>
2021-03-03 16:04:06 +0100

    Started working on Junit tests

---
+++ Client/src/test/java/com/Group3/monitor/configuration/ConfigManagerTest.java
@@ -0,0 +1,30 @@
+package com.Group3.monitor.configuration;
+
+import org.junit.Test;
+
+public class ConfigManagerTest {
+
+	@Test
+	public void testGetPropertyAsLongWithNumberValuePositive() {
+		// Setup
+		final String configPath = ConfigurationManager.getInstance().getPropertiesPath();
+		
+		
+		// Act
+		
+		
+		
+		// Assert
+		
+		
+		
+		// Clean
+	}
+	
+	
+	
+	private void cleanConfigProperties(String path) {
+		
+	}
+	
+}
