72a851ccece68e58e61ceb6a9599647bfd2d1370
byggemand-bob <byggemand-bob>
2021-03-04 16:16:50 +0100

    Create MonitorConfigException.java
    Exception type for testing

---
+++ Client/src/main/java/com/Group3/petstore/MonitorConfigException.java
@@ -0,0 +1,15 @@
+package com.Group3.petstore;
+
+public class MonitorConfigException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public MonitorConfigException (String msg) {
+		super(msg);
+	}
+	
+}
