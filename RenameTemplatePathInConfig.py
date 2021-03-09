49752d48b545b5e1b2750b16246dd853f8cd64ad
AxialDark <alex.klitgaard@gmail.com>
2021-03-09 09:27:57 +0100

    Updated build script to allow for autogeneration and adding of additional templates for Monitor Project

--- RenameTemplatePathInConfig.py
+++ RenameTemplatePathInConfig.py
@@ -26,3 +26,8 @@ ChangeTemplateInConfig(
 ChangeTemplateInConfig(
     '/home/travis/build/byggemand-bob/Group3-monitoring-distributed-systems/Client/config.yaml', 
     'templateDir: Client/src/main/resources/cstTemplates')
+
+#Change the template file path in Monitor project
+ChangeTemplateInConfig(
+    '/home/travis/build/byggemand-bob/Group3-monitoring-distributed-systems/Monitor/config.yaml', 
+    'templateDir: Monitor/src/main/resources/cstTemplates')
