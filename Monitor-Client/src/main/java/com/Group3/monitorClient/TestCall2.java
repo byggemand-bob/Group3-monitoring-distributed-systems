77abe375b70788e29ab6d2e46061c7661470cd9b
byggemand-bob <45236808+byggemand-bob@users.noreply.github.com>
2021-03-15 08:32:42 +0100

    Update TestCall2.java

--- Monitor-Client/src/main/java/com/Group3/monitorClient/TestCall2.java
+++ Monitor-Client/src/main/java/com/Group3/monitorClient/TestCall2.java
@@ -1,8 +1,8 @@
 package com.Group3.monitorClient;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.sql.*;
+
 
 @SpringBootApplication
 public class TestCall2 {
@@ -11,13 +11,13 @@ public class TestCall2 {
 
     public static void main(String[] args){
         createNewDatabase();
-        createNewTable();
+        CreateNewTable("queue", "id integer PRIMARY KEY", "name text NOT NULL", "capacity real");
         TestCall2 app = new TestCall2();
         // insert three new rows
 //        app.insert("Raw Materials", 3000);
 //        app.insert("Semifinished Goods", 4000);
 //        app.insert("Finished Goods", 5000);
-        app.selectAll("warehouses", "id", "name", "capacity");
+        System.out.println(app.CheckIfExists("queue"));
     }
     public static void createNewDatabase() {
         try (Connection conn = DriverManager.getConnection(url+fileName)) {
@@ -31,18 +31,21 @@ public class TestCall2 {
             System.out.println(e.getMessage());
         }
     }
-    public static void createNewTable() {
-        // SQL statement for creating a new table
-        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
-                + "	id integer PRIMARY KEY,\n"
-                + "	name text NOT NULL,\n"
-                + "	capacity real\n"
-                + ");";
+
+    public static void CreateNewTable(String table, String... args) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("CREATE TABLE IF NOT EXISTS ").append(table).append(" (\n");
+        for (String arg: args){
+            sql.append("\t").append(arg).append(",\n");
+        }
+        sql = new StringBuilder(sql.substring(0,sql.length()-2)).append("\n");
+        sql.append(");");
+        System.out.println(sql);
 
         try (Connection conn = DriverManager.getConnection(url+fileName);
              Statement stmt = conn.createStatement()) {
             // create a new table
-            stmt.execute(sql);
+            stmt.execute(sql.toString());
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -57,7 +60,7 @@ public class TestCall2 {
     public void insert(String name, double capacity) {
         String sql = "INSERT INTO warehouses(name,capacity) VALUES(?,?)";
 
-        try (Connection conn = this.connect();
+        try (Connection conn = this.Connect();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, name);
             pstmt.setDouble(2, capacity);
@@ -67,7 +70,7 @@ public class TestCall2 {
         }
     }
 
-    private static Connection connect() {
+    private static Connection Connect() {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url+fileName);
@@ -89,7 +92,7 @@ public class TestCall2 {
         cols = cols.substring(0, cols.length()-2);
         String sql = "SELECT "+cols+" FROM "+table;
 
-        try (Connection conn = this.connect();
+        try (Connection conn = this.Connect();
              Statement stmt  = conn.createStatement();
              ResultSet rs    = stmt.executeQuery(sql)){
             // loop through the result set
@@ -99,7 +102,46 @@ public class TestCall2 {
                         rs.getDouble("capacity"));
             }
         } catch (SQLException e) {
+            System.out.println(e.getClass());
             System.out.println(e.getMessage());
         }
     }
+
+    public boolean CheckIfExists(String tableName) {
+        String sql = "SELECT name FROM sqlite_master WHERE name = '" + tableName +"'";
+
+        try (Connection conn = this.Connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            return rs.next();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public static void createNewTable() {
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+//    public String CreateMessage(){
+//        String sqlQuery
+//        return null;
+//    }
+
+
+
 }
