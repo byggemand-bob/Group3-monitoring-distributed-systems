51f852c48950b14b1b7024bcc1ccd676a8b840ab
byggemand-bob <45236808+byggemand-bob@users.noreply.github.com>
2021-03-15 08:32:38 +0100

    Update MonitorDataMessage.java

--- Monitor-Client/src/main/java/com/Group3/monitorClient/Messenger/messagequeue/MonitorDataMessage.java
+++ Monitor-Client/src/main/java/com/Group3/monitorClient/Messenger/messagequeue/MonitorDataMessage.java
@@ -2,4 +2,8 @@ package com.Group3.monitorClient.Messenger.messagequeue;
 
 public class MonitorDataMessage implements MessageInterface{
 
+    @Override
+    public String CreateMessage() {
+        return null;
+    }
 }
