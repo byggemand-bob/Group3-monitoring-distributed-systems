edaaa703e27e7647fbd0a077e7a9058c48e7bb53
AxialDark <alex.klitgaard@gmail.com>
2021-04-08 15:58:35 +0200

    Added a task to run cleanup of old messages. Made it use a configuration property for defining the number of days to keep failed messages

---
+++ Monitor-Client/src/main/java/com/group3/scheduling/task/CleanUpOldFailedMessagesTask.java
@@ -0,0 +1,41 @@
+package com.group3.scheduling.task;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import com.group3.monitorClient.configuration.ConfigurationManager;
+import com.group3.monitorClient.messenger.messages.SQLManager;
+import com.group3.monitorClient.messenger.queue.PersistentSQLQueue;
+
+public class CleanUpOldFailedMessagesTask extends AbstractTask {
+
+	private final String cleanupSQL = "DELETE FROM queue WHERE ToBeSent = 0 AND datetime(Timestamp) < datetime('now', ?)";
+	
+	public CleanUpOldFailedMessagesTask(String name, String group, String cron) {
+		super(name, group, cron);
+	}
+	
+	public CleanUpOldFailedMessagesTask() {
+		// intentionally left empty, as it is needed in order to allow for scheduling of Task
+	}
+	
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		System.out.println("Started cleanup of old failed messages...");
+		
+		int daysToKeep = ConfigurationManager.getInstance().getPropertyAsInteger(ConfigurationManager.daysToKeepMessages);
+		
+		PersistentSQLQueue sqlQueue = new PersistentSQLQueue(PersistentSQLQueue.queueDBPath, PersistentSQLQueue.queueDBFile);
+		int removedMessagesCount = sqlQueue.cleanupOldMessages(cleanupSQL, daysToKeep);		
+		
+		if (removedMessagesCount > -1) {
+			System.out.println("Finished cleanup of old failed message, removed <" + removedMessagesCount + "> old messages...");			
+		} else {
+			System.err.println("Failed in running cleanup of old failed messages!!!");
+		}
+		
+		// Always remember to close connection
+		sqlQueue.CloseConnection();
+	}
+
+}
