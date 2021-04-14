359de94b793d3b2497cff52c9e03e776e5b7b1db
AxialDark <alex.klitgaard@gmail.com>
2021-04-14 10:33:12 +0200

    Setup the built-in cleanup task to be scheduled when starting the monitor-server

--- Monitor/src/main/java/com/group3/monitorServer/MonitorServerApplication.java
+++ Monitor/src/main/java/com/group3/monitorServer/MonitorServerApplication.java
@@ -1,13 +1,21 @@
 package com.group3.monitorServer;
 
+import org.quartz.SchedulerException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.group3.monitorClient.scheduling.TaskScheduler;
 
 @SpringBootApplication
 public class MonitorServerApplication {
 
-	public static void main(String[] args) {
+	private static TaskScheduler scheduler;
+	
+	public static void main(String[] args) throws SchedulerException {
 		SpringApplication.run(MonitorServerApplication.class, args);
+		
+		scheduler = new TaskScheduler();
+		scheduler.Initialize();
 	}
 
 }
