d24aadb13468eb87cfb29ab522676bb82a10a09f
Kristian Walstrøm Petersen <43957853+KristianWP@users.noreply.github.com>
2021-04-15 15:00:34 +0200

    updated messenger test classes

--- Monitor-Client/src/test/java/com/group3/monitorClient/testClasses/GreedyMessenger_TestClass.java
+++ Monitor-Client/src/test/java/com/group3/monitorClient/testClasses/GreedyMessenger_TestClass.java
@@ -1,28 +1,27 @@
 package com.group3.monitorClient.testClasses;
 
-import com.group3.monitorClient.controller.MonitorClientInterface;
 import com.group3.monitorClient.messenger.GreedyMessenger;
-import com.group3.monitorClient.messenger.messages.MessageInterface;
-import com.group3.monitorClient.messenger.queue.QueueInterface;
-import com.group3.monitorClient.messenger.queue.SynchronizedQueue;
-
 /*
  * Test class set monitorClient in GreedyMessenger to MonitorClient_TestClass,
  * this removes the need for connecting to a server
  */
 public class GreedyMessenger_TestClass extends GreedyMessenger {
-    public GreedyMessenger_TestClass(String monitorIP, QueueInterface<MessageInterface> messageQueue) {
-        super(monitorIP, messageQueue);
+    public GreedyMessenger_TestClass(String monitorIP, String sqlPath, String sqlFileName) {
+        super(monitorIP, sqlPath, sqlFileName);
         monitorClientInterface = new MonitorClientInterface_TestClass(monitorIP);
     }
 
     /* Specifies the http status code returned when messages are being sent */
-    public GreedyMessenger_TestClass(String monitorIP, QueueInterface<MessageInterface> messageQueue, int ReturnStatusCode) {
-        super(monitorIP, messageQueue);
+    public GreedyMessenger_TestClass(String monitorIP, String sqlPath, String sqlFileName, int ReturnStatusCode) {
+        super(monitorIP, sqlPath, sqlFileName);
         monitorClientInterface = new MonitorClientInterface_TestClass(monitorIP, ReturnStatusCode);
     }
 
     public void ChangeReturnedStatusCode(int StatusCode){
         ((MonitorClientInterface_TestClass)monitorClientInterface).ChangeReturnedStatusCode(StatusCode);
+    }
+
+    public void CloseConnection(){
+        sqlMessageManager.CloseConnection();
     }
 }
--- Monitor-Client/src/test/java/com/group3/monitorClient/testClasses/LazyMessenger_TestClass.java
+++ Monitor-Client/src/test/java/com/group3/monitorClient/testClasses/LazyMessenger_TestClass.java
@@ -1,16 +1,20 @@
 package com.group3.monitorClient.testClasses;
 
+import com.group3.monitorClient.messenger.GreedyMessenger;
 import com.group3.monitorClient.messenger.lazyMessenger.LazyMessenger;
-import com.group3.monitorClient.messenger.messages.MessageInterface;
-import com.group3.monitorClient.messenger.queue.SynchronizedQueue;
 
 /*
  * Test class set subMessenger in LazyMessenger to GreedyMessenger_TestClass,
  * this removes the need for connecting to a server
  */
 public class LazyMessenger_TestClass extends LazyMessenger {
-    public LazyMessenger_TestClass(String monitorIP, SynchronizedQueue<MessageInterface> messageQueue) {
-        super(monitorIP, messageQueue);
-        subMessenger = new GreedyMessenger_TestClass(monitorIP, messageQueue);
+    public LazyMessenger_TestClass(String monitorIP, String sqlPath, String sqlFileName) {
+        super(monitorIP, sqlPath, sqlFileName);
+        ((GreedyMessenger)subMessenger).CloseConnectionToSQL();
+        subMessenger = new GreedyMessenger_TestClass(monitorIP, sqlPath, sqlFileName);
+    }
+
+    public void CloseConnection(){
+        ((GreedyMessenger_TestClass)subMessenger).CloseConnection();
     }
 }
