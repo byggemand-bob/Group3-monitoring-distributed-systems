5b04e7a1f630973db08a8e861ddb9c13d11d1376
Kristian Walstrøm Petersen <43957853+KristianWP@users.noreply.github.com>
2021-04-15 14:59:38 +0200

    Handlers for deleting and creating SQLdatabases before and after testing

---
+++ Monitor-Client/src/test/java/com/group3/monitorClient/AbstractSQLMessageManagerTest.java
@@ -0,0 +1,80 @@
+package com.group3.monitorClient;
+
+import com.group3.monitorClient.messenger.messages.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openapitools.client.model.ErrorData;
+import org.openapitools.client.model.TimingMonitorData;
+import org.threeten.bp.OffsetDateTime;
+
+import java.io.File;
+
+public class AbstractSQLMessageManagerTest {
+    public static SQLMessageManager sqlMessageManager;// = new SQLManager("src/main/resources/sqlite/db/", "test.db");
+
+    @BeforeAll
+    static void setupDir() {
+        File path = new File("src/main/resources/sqlite/db/");
+        path.mkdirs();
+
+        File db = new File("src/main/resources/sqlite/db/test.db");
+        if (db.delete()) {
+            System.out.println("there was already a test.db at location, and it was deleted");
+        }
+    }
+
+    /* Creates a new sql database before each test */
+    @BeforeEach
+    public void setup () {
+        SQLManager sqlManager = new SQLManager("src/main/resources/sqlite/db/", "test.db");
+        sqlMessageManager = new SQLMessageManager(sqlManager, "Messages");
+    }
+
+    /* Deletes the sql database after each test */
+    @AfterEach
+    public void cleanUp() {
+        File db = new File(getSQLPath() + getSQLFileName());
+        sqlMessageManager.CloseConnection();
+
+        if (!db.delete()) {
+            System.out.println(this.getClass().toString() + ".AbstractSQLMessageManagerTest: Could not access database file. It was therefore not deleted!");
+        }
+    }
+
+    public TimingMonitorDataMessage getDefaultTimingMessage(){
+        TimingMonitorData timingMonitorData = new TimingMonitorData();
+        timingMonitorData.setEventCode(TimingMonitorData.EventCodeEnum.RECEIVEREQUEST);
+        timingMonitorData.setTimestamp(OffsetDateTime.now());
+        timingMonitorData.setTargetEndpoint("/test");
+        timingMonitorData.setEventID(99L);
+        timingMonitorData.setSenderID(100L);
+
+        MessageCreator messageCreator = new MessageCreator();
+
+        MessageInterface timingMessage = messageCreator.MakeMessage(timingMonitorData);
+        return (TimingMonitorDataMessage) timingMessage;
+    }
+
+    public ErrorDataMessage getDefaultErrorMessage(){
+        ErrorData errorData = new ErrorData();
+        errorData.setErrorMessageType(ErrorData.ErrorMessageTypeEnum.HTTPERROR);
+        errorData.setSenderID(100L);
+        errorData.setTimestamp(OffsetDateTime.now());
+        errorData.setComment("test");
+        errorData.setHttpResponse(200);
+        return new ErrorDataMessage(errorData);
+    }
+
+    public String getSQLPath(){
+        return sqlMessageManager.getPath();
+    }
+
+    public String getSQLFileName(){
+        return sqlMessageManager.getFileName();
+    }
+
+    public String getSQLTableName(){
+        return sqlMessageManager.getTableName();
+    }
+}
