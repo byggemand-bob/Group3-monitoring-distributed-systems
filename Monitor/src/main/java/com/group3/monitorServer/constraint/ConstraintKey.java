d6bef6b0a347b2e3774f00169428e69c9d4fd22b
AxialDark <alex.klitgaard@gmail.com>
2021-04-16 11:27:47 +0200

    Finished the ConstraintKey tests. Made sure that we do not generate the same hashcode if the nodeID is null and the nodeID value is 0 (instead nodeID value should never be negative)

--- Monitor/src/main/java/com/group3/monitorServer/constraint/ConstraintKey.java
+++ Monitor/src/main/java/com/group3/monitorServer/constraint/ConstraintKey.java
@@ -62,7 +62,7 @@ public class ConstraintKey {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((nodeID == null) ? 0 : nodeID.intValue());
+		result = prime * result + ((nodeID == null) ? -1 : nodeID.intValue());
 		result = prime * result + endpoint.hashCode();
 		return result;
 	}
