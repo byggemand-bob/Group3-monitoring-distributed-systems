7d096953626aa4676fe9017b76bedd0a5b369bff
Kristian Walstrøm Petersen <43957853+KristianWP@users.noreply.github.com>
2021-04-22 09:42:06 +0200

    moved relevant tests from old processor_test to Delegator_Tests

--- Monitor/src/test/java/com/group3/monitorServer/messageProcessor/Delegator_Tests.java
+++ Monitor/src/test/java/com/group3/monitorServer/messageProcessor/Delegator_Tests.java
@@ -1,12 +1,19 @@
 package com.group3.monitorServer.messageProcessor;
 
 import com.group3.monitorServer.messages.ErrorDataMessage;
+import com.group3.monitorServer.messages.MessageCreator;
 import com.group3.monitorServer.messages.SQLMessageManager;
 import com.group3.monitorServer.messages.TimingMonitorDataMessage;
 import com.group3.monitorServer.testClasses.AbstractSQLMessageManagerTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openapitools.model.TimingMonitorData;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.OffsetDateTime;
 
 public class Delegator_Tests extends AbstractSQLMessageManagerTest {
     /* Tests the control functions start(), stop(), pause() and resume() */
@@ -89,5 +96,99 @@ public class Delegator_Tests extends AbstractSQLMessageManagerTest {
         //msg7
         timingMonitorDataMessage.getTimingMonitorData().setEventCode(TimingMonitorData.EventCodeEnum.SENDRESPONSE);
         timingMonitorDataMessage.makeSQL(sqlMessageManager);
+    }
+
+    @Test
+    public void blobTest () throws SQLException {
+        sqlMessageManager.InsertMessage(21L, 1, "test", "messageTest1");
+        sqlMessageManager.InsertMessage(21L, 1, "test", "messageTest2");
+        sqlMessageManager.InsertMessage(21L, 1, "test", "messageTest3");
+        sqlMessageManager.InsertMessage(21L, 1, "test", "messageTest4");
+
+        //Act
+        ResultSet rs = sqlMessageManager.SelectMessages("Message = 'messageTest3'");
+
+
+
+        //Assert
+        Assertions.assertEquals("messageTest3", rs.getString("Message"));
+        sqlMessageManager.CloseConnection();
+    }
+
+    @Test
+    public void testFindTimingDataMatchPass () throws SQLException { //TODO: refactor to match returns of the new analyzed messages
+        //Setup
+        MessageCreator messageCreator = new MessageCreator();
+        OffsetDateTime offsetDateTime = OffsetDateTime.now();
+
+        //Message 1
+        TimingMonitorData timingMonitorData = new TimingMonitorData();
+        timingMonitorData.setTimestamp(offsetDateTime);
+        timingMonitorData.setTargetEndpoint("/monitor");
+        timingMonitorData.setEventID(22L);
+        timingMonitorData.setSenderID(2L);
+        timingMonitorData.setEventCode(TimingMonitorData.EventCodeEnum.SENDREQUEST);
+        String blob = timingMonitorData.getTargetEndpoint() + TimingMonitorDataMessage.separator +
+                timingMonitorData.getEventID() + TimingMonitorDataMessage.separator +
+                timingMonitorData.getEventCode().ordinal();
+        sqlMessageManager.InsertMessage(timingMonitorData.getSenderID(), 0, offsetDateTime.toString(), blob);
+
+
+        //Message 2
+        timingMonitorData.setTimestamp(offsetDateTime);
+        timingMonitorData.setTargetEndpoint("/monitor");
+        timingMonitorData.setEventID(24L);
+        timingMonitorData.setSenderID(2L);
+        timingMonitorData.setEventCode(TimingMonitorData.EventCodeEnum.SENDREQUEST);
+        blob = timingMonitorData.getTargetEndpoint() + TimingMonitorDataMessage.separator +
+                timingMonitorData.getEventID() + TimingMonitorDataMessage.separator +
+                timingMonitorData.getEventCode().ordinal();
+        sqlMessageManager.InsertMessage(timingMonitorData.getSenderID(), 0, offsetDateTime.toString(), blob);
+
+        //Message 3
+        timingMonitorData.setTimestamp(offsetDateTime);
+        timingMonitorData.setTargetEndpoint("/monitor");
+        timingMonitorData.setEventID(23L);
+        timingMonitorData.setSenderID(2L);
+        timingMonitorData.setEventCode(TimingMonitorData.EventCodeEnum.SENDREQUEST);
+        blob = timingMonitorData.getTargetEndpoint() + TimingMonitorDataMessage.separator +
+                timingMonitorData.getEventID() + TimingMonitorDataMessage.separator +
+                timingMonitorData.getEventCode().ordinal();
+        sqlMessageManager.InsertMessage(timingMonitorData.getSenderID(), 0, offsetDateTime.toString(), blob);
+
+        //Message 4
+        timingMonitorData.setTimestamp(offsetDateTime);
+        timingMonitorData.setTargetEndpoint("/monitor");
+        timingMonitorData.setEventID(22L);
+        timingMonitorData.setSenderID(2L);
+        timingMonitorData.setEventCode(TimingMonitorData.EventCodeEnum.RECEIVERESPONSE);
+        blob = timingMonitorData.getTargetEndpoint() + TimingMonitorDataMessage.separator +
+                timingMonitorData.getEventID() + TimingMonitorDataMessage.separator +
+                timingMonitorData.getEventCode().ordinal();
+        sqlMessageManager.InsertMessage(timingMonitorData.getSenderID(), 0, offsetDateTime.toString(), blob);
+
+        //Act
+        TimingMonitorDataMessage message = null;
+        TimingMonitorDataMessage matchingMessage = null;
+        try {
+            Method method = null;
+            method = Delegator.class.getDeclaredMethod("findTimingDataMatch", TimingMonitorDataMessage.class);
+            method.setAccessible(true);
+            Delegator delegator = new Delegator(sqlMessageManager);
+            message = (TimingMonitorDataMessage) messageCreator.MakeMessage(timingMonitorData);
+            matchingMessage = ((TimingMonitorDataMessageID) method.invoke(delegator, message)).timingMonitorDataMessage;
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        //Assert
+        Assertions.assertEquals(message.getTimingMonitorData().getEventID(),matchingMessage.getTimingMonitorData().getEventID());
+        Assertions.assertEquals(message.getTimingMonitorData().getSenderID(),matchingMessage.getTimingMonitorData().getSenderID());
+        Assertions.assertNotEquals(message.getTimingMonitorData().getEventCode(),matchingMessage.getTimingMonitorData().getEventCode());
+        Assertions.assertEquals(TimingMonitorData.EventCodeEnum.SENDREQUEST,matchingMessage.getTimingMonitorData().getEventCode());
     }
 }
