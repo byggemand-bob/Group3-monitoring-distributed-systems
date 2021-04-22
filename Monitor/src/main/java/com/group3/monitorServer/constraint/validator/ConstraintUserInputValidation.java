90913f17c34149774ce5e29cc679ddc1e408c548
Stensgaard <stensgaard_@outlook.com>
2021-04-22 14:54:10 +0200

    finished ConstraintUserInputValidation and added some junit tests for it

--- Monitor/src/main/java/com/group3/monitorServer/constraint/validator/ConstraintUserInputValidation.java
+++ Monitor/src/main/java/com/group3/monitorServer/constraint/validator/ConstraintUserInputValidation.java
@@ -4,35 +4,36 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
+import com.group3.monitorServer.constraint.Constraint;
+
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+/**
+ * Responsible for checking the structure of the JSON specification file containing the {@link Constraint}
+ */
 public class ConstraintUserInputValidation {
 
-	private final String schemaJSON = "..\\Monitor\\src\\main\\resources\\cstTemplates\\validation\\constraintSchema.json";
+	private final String schemaJSON = "..\\Monitor\\src\\main\\resources\\validation\\constraintSchema.json";
 	
-	public void validate(String jsonFilePath) {
+	/**
+	 * Validate the JSON specification file against the schema
+	 * Returns an exception if there was a error validating the JSON specification file
+	 * 
+	 * @param jsonFilePath the file path for the JSON specification file
+	 * @throws FileNotFoundException Throws a exception if the JSON files could not be found
+	 */
+	public void validate(String jsonFilePath) throws FileNotFoundException {
 
-		InputStream schemaInputStream = null;
-		//TODO fix this 
-		InputStream dataInputStream = null;
+		InputStream schemaInputStream = new FileInputStream(schemaJSON);
+		InputStream dataInputStream = new FileInputStream(jsonFilePath);
 
-		System.out.println(jsonFilePath);
-
-		try {
-			schemaInputStream = new FileInputStream(schemaJSON);
-			dataInputStream = new FileInputStream(jsonFilePath);
-		}
-		catch (FileNotFoundException e) {
-			System.err.println(e.getMessage());
-		}
-		
 		System.out.println("Started to validate JSON file");
-	    JSONObject jsonSchema = new JSONObject(schemaInputStream);
-		JSONArray jsonSubject = new JSONArray(new JSONTokener(ConstraintUserInputValidation.class.getResourceAsStream("/testData.json")));
+	    JSONObject jsonSchema = new JSONObject(new JSONTokener(schemaInputStream));
+		JSONArray jsonSubject = new JSONArray(new JSONTokener(dataInputStream));
 
 		SchemaLoader loader = SchemaLoader
 			.builder()
--- Monitor/src/test/java/com/group3/monitorServer/constraint/validator/ConstraintUserInputValidationTest.java
+++ Monitor/src/test/java/com/group3/monitorServer/constraint/validator/ConstraintUserInputValidationTest.java
@@ -1,18 +1,70 @@
 package com.group3.monitorServer.constraint.validator;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+
+import org.everit.json.schema.ValidationException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ConstraintUserInputValidationTest {
 	
-	//make tests for validateJson
+	private final String data = "..\\Monitor\\src\\test\\java\\com\\group3\\monitorServer\\constraint\\validator\\testFiles\\";
+	ConstraintUserInputValidation inputValidation = new ConstraintUserInputValidation();
 	
 	@Test
-	void validateJsonCorrectFile() {
+	void validateCorrectFile() throws FileNotFoundException {
+		//setup
+		final String fileName = "CorrectData.json";
+		//act
+		inputValidation.validate(data + fileName);
 		
 	}
 	
 	@Test
-	void validateJsonIncorrectFile() {
-		
+	void validateIncorrectFile() throws FileNotFoundException {
+		//setup
+		final String fileName = "InCorrectData.json";
+		//assert
+		Assertions.assertThrows(ValidationException.class, () -> {
+			inputValidation.validate(data + fileName);
+		});
+	}
+	
+	@Test
+	void ValidateEmptyFile() throws FileNotFoundException {
+		//setup
+		final String fileName = "EmptyArrayData.json";
+		//act
+		inputValidation.validate(data + fileName);
+	}
+	
+	@Test
+	void ValidateCorrectFileWithOptionalValue() throws FileNotFoundException {
+		//setup
+		final String fileName = "CorrectDataWithOptionalValue.json";
+		//act
+		inputValidation.validate(data + fileName);
+	}
+	
+	@Test
+	void ValidateInCorrectFileWithOptionalValue() throws FileNotFoundException {
+		//setup
+		final String fileName = "InCorrectDataWithOptionalValue.json";
+		//assert
+		Assertions.assertThrows(ValidationException.class, () -> {
+			inputValidation.validate(data + fileName);
+		});
+	}
+	
+	@Test
+	void ValidateCantFindFile() throws FileNotFoundException {
+		//setup
+		final String fileName = "CantFindThisFile.json";
+		//assert
+		Assertions.assertThrows(FileNotFoundException.class, () -> {
+			inputValidation.validate(data + fileName);
+		});
 	}
 }
