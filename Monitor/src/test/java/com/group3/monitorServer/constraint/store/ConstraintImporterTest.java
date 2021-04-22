7a3520fcbc41ef53caa6d37f7c04bda978f0cd9b
Stensgaard <stensgaard_@outlook.com>
2021-04-22 11:12:01 +0200

    added junit test suites

--- Monitor/src/test/java/com/group3/monitorServer/constraint/store/ConstraintImporterTest.java
+++ Monitor/src/test/java/com/group3/monitorServer/constraint/store/ConstraintImporterTest.java
@@ -11,11 +11,6 @@ public class ConstraintImporterTest {
 	
 	@Test
 	void importConstraintsWithWrongFilePath() {
-		
-	}
-	
-	@Test
-	void importConstraintsWithWrongFileThrowExecptionTest() {
-		
+		//filenotfound execption
 	}
 }
---
+++ Monitor/src/test/java/com/group3/monitorServer/constraint/validator/ConstraintUserInputValidationTest.java
@@ -0,0 +1,18 @@
+package com.group3.monitorServer.constraint.validator;
+
+import org.junit.jupiter.api.Test;
+
+public class ConstraintUserInputValidationTest {
+	
+	//make tests for validateJson
+	
+	@Test
+	void validateJsonCorrectFile() {
+		
+	}
+	
+	@Test
+	void validateJsonIncorrectFile() {
+		
+	}
+}
