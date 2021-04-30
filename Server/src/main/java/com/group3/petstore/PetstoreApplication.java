4e81cbc963995304268554ab62109b4397d7c94d
byggemand-bob <45236808+byggemand-bob@users.noreply.github.com>
2021-04-30 17:26:37 +0200

    Server update

--- Server/src/main/java/com/group3/petstore/PetstoreApplication.java
+++ Server/src/main/java/com/group3/petstore/PetstoreApplication.java
@@ -1,6 +1,8 @@
 package com.group3.petstore;
 
 import com.group3.monitorClient.MonitorClientInterface;
+import com.group3.monitorClient.controller.Controller;
+import com.group3.monitorClient.controller.requirements.AvailableCPURequirement;
 import com.group3.monitorClient.messenger.Messenger;
 import com.group3.monitorClient.messenger.messages.SQLManager;
 import com.group3.monitorClient.messenger.messages.SQLMessageManager;
@@ -16,7 +18,9 @@ public class PetstoreApplication {
 	public static void main(String[] args) {
 		Messenger.initialize("src/main/resources/sqlite/db", "queue.db");//TODO: User-specification paths instead
 		Messenger.getInstance().start();//TODO: add to some autogeneration
-		//TODO: addController
+		Controller controller = new Controller();
+		controller.addRequirement(new AvailableCPURequirement(0)); //TODO: Should be added to the configuration.properties and configurationManger such that i can handle doubles.
+		controller.addThread(Messenger.getInstance());
 		SpringApplication.run(PetstoreApplication.class, args);
 
 	}
