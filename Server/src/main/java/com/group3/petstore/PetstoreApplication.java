764ffda2d95d9263e73aa9aa57e0964ac1a3422d
Anders Sønderby <45236808+byggemand-bob@users.noreply.github.com>
2021-05-04 13:33:40 +0200

    Update PetstoreApplication.java

--- Server/src/main/java/com/group3/petstore/PetstoreApplication.java
+++ Server/src/main/java/com/group3/petstore/PetstoreApplication.java
@@ -1,5 +1,6 @@
 package com.group3.petstore;
 
+import com.group3.monitorClient.configuration.ConfigurationManager;
 import com.group3.monitorClient.controller.Controller;
 import com.group3.monitorClient.controller.requirements.AvailableCPURequirement;
 import com.group3.monitorClient.messenger.Messenger;
@@ -14,13 +15,11 @@ import org.springframework.context.annotation.ComponentScan;
 public class PetstoreApplication {
 
 	public static void main(String[] args) {
-		Messenger.initialize("src" + File.separator + "main" + File.separator + "resources" + File.separator + "sqlite" + File.separator + "db" + File.separator, "queue.db");//TODO: User-specification paths instead
 		Messenger.getInstance().start();
 		Controller controller = new Controller();
-		controller.addRequirement(new AvailableCPURequirement(0)); //TODO: Should be added to the configuration.properties and configurationManger such that i can handle doubles.
+		controller.addRequirement(new AvailableCPURequirement(ConfigurationManager.getInstance().getPropertyAsDouble(ConfigurationManager.availableCPURequirementProp, 0.2)));
 		controller.addThread(Messenger.getInstance());
 		controller.start();
 		SpringApplication.run(PetstoreApplication.class, args);
-
 	}
 }
