88ce11c0eac3f27f56fcb2da6616a5802154b084
AxialDark <alex.klitgaard@gmail.com>
2021-05-04 12:55:30 +0200

    Made paths platform independent and removed unused imports

--- Server/src/main/java/com/group3/petstore/PetstoreApplication.java
+++ Server/src/main/java/com/group3/petstore/PetstoreApplication.java
@@ -1,23 +1,21 @@
 package com.group3.petstore;
 
-import com.group3.monitorClient.MonitorClientInterface;
 import com.group3.monitorClient.controller.Controller;
 import com.group3.monitorClient.controller.requirements.AvailableCPURequirement;
 import com.group3.monitorClient.messenger.Messenger;
-import com.group3.monitorClient.messenger.messages.SQLManager;
-import com.group3.monitorClient.messenger.messages.SQLMessageManager;
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "org.openapitools.api")
 public class PetstoreApplication {
 
 	public static void main(String[] args) {
-		Messenger.initialize("src/main/resources/sqlite/db/", "queue.db");//TODO: User-specification paths instead
-		Messenger.getInstance().start();//TODO: add to some autogeneration
+		Messenger.initialize("src" + File.separator + "main" + File.separator + "resources" + File.separator + "sqlite" + File.separator + "db" + File.separator, "queue.db");//TODO: User-specification paths instead
+		Messenger.getInstance().start();
 		Controller controller = new Controller();
 		controller.addRequirement(new AvailableCPURequirement(0)); //TODO: Should be added to the configuration.properties and configurationManger such that i can handle doubles.
 		controller.addThread(Messenger.getInstance());
