434b32f5d3619faeb659b8d7fa8c642f0798ae1e
AxialDark <alex.klitgaard@gmail.com>
2021-05-10 13:27:04 +0200

    Setup former "client" project to implement system tests

---
+++ Client/src/main/java/com/group3/tester/SystemTester.java
@@ -0,0 +1,8 @@
+package com.group3.tester;
+
+public class SystemTester {
+
+	public static void main(String[] args) {
+		
+	}
+}
--- Client/src/main/java/com/group3/tester/SystemTesterApplication.java
+++ Client/src/main/java/com/group3/tester/SystemTesterApplication.java
@@ -1,4 +1,4 @@
-package com.group3.petstore;
+package com.group3.tester;
 
 import com.group3.monitorClient.configuration.ConfigurationManager;
 import com.group3.monitorClient.controller.Controller;
@@ -11,13 +11,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.IOException;
 
 @SpringBootApplication
-public class PetstoreApplication {
+public class SystemTesterApplication {
 
 	public static void main(String[] args) throws IOException, ApiException {
-		Controller controller = new Controller();
-		controller.addRequirement(new AvailableCPURequirement(ConfigurationManager.getInstance().getPropertyAsDouble(ConfigurationManager.availableCPURequirementProp, 0.2)));
-		controller.addThread(Messenger.getInstance());
-		controller.start();
-		SpringApplication.run(PetstoreApplication.class, args);
+		SpringApplication.run(SystemTesterApplication.class, args);
 	}
 }
+
+//Controller controller = new Controller();
+//controller.addRequirement(new AvailableCPURequirement(ConfigurationManager.getInstance().getPropertyAsDouble(ConfigurationManager.availableCPURequirementProp, 0.2)));
+//controller.addThread(Messenger.getInstance());
+//controller.start();
