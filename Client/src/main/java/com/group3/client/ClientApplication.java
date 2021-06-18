5e0361f2576746a0389cd0020a18f492089d93ad
AxialDark <alex.klitgaard@gmail.com>
2021-06-18 09:02:04 +0200

    Made a few cosmetic changes to make the framework demo more presentable

---
+++ Client/src/main/java/com/group3/client/ClientApplication.java
@@ -0,0 +1,23 @@
+package com.group3.client;
+
+import com.group3.monitorClient.configuration.ConfigurationManager;
+import com.group3.monitorClient.controller.Controller;
+import com.group3.monitorClient.controller.requirements.AvailableCPURequirement;
+import com.group3.monitorClient.messenger.Messenger;
+import org.openapitools.client.ApiException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) throws IOException, ApiException {
+		Controller controller = new Controller();
+		controller.addRequirement(new AvailableCPURequirement(ConfigurationManager.getInstance().getPropertyAsDouble(ConfigurationManager.availableCPURequirementProp, 0.2)));
+		controller.addThread(Messenger.getInstance());
+		controller.start();
+		SpringApplication.run(ClientApplication.class, args);
+	}
+}
